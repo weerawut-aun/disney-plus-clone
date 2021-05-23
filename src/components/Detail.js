0ea972715bf02acc6a42ec4012ddbd05e2ebf0ce
weerawut-aun <weerawut2016@outlook.co.th>
2021-05-23 20:58:18 +0700

    ADD.detail page

---
+++ src/components/Detail.js
@@ -0,0 +1,145 @@
+import React from 'react';
+import styled from 'styled-components';
+
+function Detail() {
+  return (
+    <Container>
+      <Background>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/4F39B7E16726ECF419DD7C49E011DD95099AA20A962B0B10AA1881A70661CE45/scale?width=1440&aspectRatio=1.78&format=jpeg" />
+      </Background>
+      <ImageTitle>
+        <img src="https://prod-ripcut-delivery.disney-plus.net/v1/variant/disney/D7AEE1F05D10FC37C873176AAA26F777FC1B71E7A6563F36C6B1B497CAB1CEC2/scale?width=1440&aspectRatio=1.78" />
+      </ImageTitle>
+      <Controls>
+        <PlayButton>
+          <img src="/images/play-icon-black.png" />
+          <span>PLAY</span>
+        </PlayButton>
+        <TrailerButton>
+          <img src="/images/play-icon-white.png" />
+          <span>Tailer</span>
+        </TrailerButton>
+        <AddButton>
+          <span>+</span>
+        </AddButton>
+        <GroupButtom>
+          <img src="/images/group-icon.png" />
+        </GroupButtom>
+      </Controls>
+      <SubTittle>2018 7m Family, Fantasy, Kids, Animation</SubTittle>
+      <Description>
+        In “Bao,” an aging Chinese mom suffering from empty nest syndrome gets
+        another chance at motherhood when one of her dumplings springs to life
+        as a lively, giggly dumpling boy. Mom excitedly welcomes this new bundle
+        of joy into her life, but Dumpling starts growing up fast, and Mom must
+        come to the bittersweet revelation that nothing stays cute and small
+        forever. This short film from Pixar Animation Studios and director Domee
+        Shi explores the ups and downs of the parent-child relationship through
+        the colorful, rich, and tasty lens of the Chinese immigrant community in
+        Canada.
+      </Description>
+    </Container>
+  );
+}
+
+export default Detail;
+
+const Container = styled.div`
+  min-height: caclc(100vh - 70px);
+  padding: 0 calc(3.5vw + 5px);
+  position: relative;
+`;
+
+const Background = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+  z-index: -1;
+  opactity: 0.8;
+
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+`;
+
+const ImageTitle = styled.div`
+  height: 30vh;
+  min-height: 170px;
+  width: 35vw;
+  min-width: 200px;
+  img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+`;
+
+const Controls = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const PlayButton = styled.button`
+  border-radius: 4px;
+  font-size: 15px;
+  padding: 6px 24px;
+  margin-right: 22px;
+  display: flex;
+  align-items: center;
+  height: 50px;
+  background: rgb(249, 249, 249);
+  borderL none;
+  letter-spacing: 1.8px;
+  cursor: pointer;
+
+  &:hover {
+      background: rgb(198,198,198);
+  }
+`;
+
+const TrailerButton = styled(PlayButton)`
+  background: rgba(0, 0, 0, 0.3);
+  border: 1px solid rgb(249, 249, 249);
+  color: rgb(249, 249, 249);
+  text-transform: uppercase;
+`;
+
+const AddButton = styled.button`
+  margin-right: 16px;
+  width: 44px;
+  height: 44px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 50%;
+  border: 2px solid white;
+  background-color: rgba(0, 0, 0, 0.6);
+  cursor: pointer;
+
+  span {
+    font-size: 30px;
+    color: white;
+  }
+`;
+
+const GroupButtom = styled(AddButton)`
+  background: rgb(0, 0, 0);
+`;
+
+const SubTittle = styled.div`
+  color: rgb(249, 249, 249);
+  font-size: 15px;
+  min-height: 20px;
+  margin-top: 26px;
+`;
+
+const Description = styled.div`
+  line-height: 1.4;
+  font-size: 20px;
+  margin-top: 16px;
+  color: rgb(249, 249, 249);
+`;
